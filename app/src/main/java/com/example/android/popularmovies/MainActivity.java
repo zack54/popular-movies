c0e6ef3b48b7acf96640f815e31cc163916518aa
zack54 <shninder5454@gmail.com>
2018-05-10 01:51:22 +0200

    feat: Add options menu

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.example.android.popularmovies.model.Movie;
@@ -17,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
     private RecyclerViewAdapter mRecyclerViewAdapter;
+
+    private String currentCriteria;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +43,10 @@ public class MainActivity extends AppCompatActivity {
      * Used to Start a Background Task & Get Data in the background.
      */
     private void loadData(String sortCriteria) {
-        new FetchTask().execute(sortCriteria);
+        if (!sortCriteria.equals(currentCriteria)) {
+            new FetchTask().execute(sortCriteria);
+        }
+
     }
 
     /**
@@ -78,4 +85,25 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.sort_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_popular:
+                loadData(NetworkUtils.getPopularSortCriteria());
+                return true;
+            case R.id.action_top_rated:
+                loadData(NetworkUtils.getTopRatedSortCriteria());
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
 }
